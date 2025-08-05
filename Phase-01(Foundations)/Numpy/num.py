76843238458dfd479119aa8f484056c60a899d82
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-08-05 11:37:06 +0600

    numpy

---
+++ Phase-01(Foundations)/Numpy/num.py
@@ -0,0 +1,6 @@
+import numpy as np 
+temperature =np.array([25, 30, 22, 28, 26, 31, 29])
+
+average_temp = np.mean(temperature)
+print( "The average temperature is : ", average_temp )
+print( "The average temperature is : "+ str(average_temp ))
