4fa93ba7d6687a5df30e159764f96f9506868937
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-08-05 13:31:03 +0600

    indexing and slicing

---
+++ Phase-01(Foundations)/Numpy/Indexing_Slicing/indexing.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# creating arrays from python lists
+arr= np.array([1, 2, 3, 4, 5])
+# usually like normal array indexing
+print(arr[1])
+print(arr[2]+arr[3])  # adding two elements
