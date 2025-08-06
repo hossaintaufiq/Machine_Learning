448f91de090ae747e5c5f9d69d36bf727c251c9b
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-08-06 21:45:23 +0600

    basic matplotlib

--- Phase-01(Foundations)/Matplotlib/simple_plot.py
+++ Phase-01(Foundations)/Matplotlib/simple_plot.py
@@ -1,9 +1,14 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.linspace(0, 2 * np.pi, 200)
+# y = np.cos(x)
+
+# fig, ax = plt.subplots()
+# ax.plot(x, y)
+# plt.show()
 import matplotlib.pyplot as plt
 import numpy as np
-
-x = np.linspace(0, 2 * np.pi, 200)
-y = np.cos(x)
-
-fig, ax = plt.subplots()
-ax.plot(x, y)
-plt.show()
+fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
+plt.show()                           # Show the figure.
