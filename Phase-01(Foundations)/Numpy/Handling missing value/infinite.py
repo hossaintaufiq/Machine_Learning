a0deb6bbc120e821b3eb8e405ee0e2c298751cce
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-08-08 12:29:50 +0600

    numpy completed , capstone project will be done after pandas library

--- Phase-01(Foundations)/Numpy/Handling missing value/infinite.py
+++ Phase-01(Foundations)/Numpy/Handling missing value/infinite.py
@@ -6,3 +6,7 @@ import numpy as np
 arr= np.array([1, 2, np.inf, 4, 5, -np.inf])
 
 print(np.isinf(arr))    
+
+cleaned_arr= np.nan_to_num(arr, posinf=1000, neginf=-1000)  # Replace inf with 1000 and -inf with -1000
+
+print(cleaned_arr)  # Output: [  1.   2. 1000.   4.   5. -1000.]
