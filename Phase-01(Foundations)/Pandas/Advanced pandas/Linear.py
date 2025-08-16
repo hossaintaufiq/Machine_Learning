81215f9129b94986239fc02ba0ba203632ed81df
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-08-16 11:21:55 +0600

    Sorting and aggrefation

--- Phase-01(Foundations)/Pandas/Advanced pandas/Linear.py
+++ Phase-01(Foundations)/Pandas/Advanced pandas/Linear.py
@@ -2,4 +2,28 @@ import pandas as pd
 
 data={
     "time": [1,2,3,4,5,6],
-    "value": [10, None, 30, None, 50, 60]}
+    "value": [10, None, 30, None, 50, 60]
+    }
+
+df=pd.DataFrame(data)
+
+print("Before Interpolation")
+
+print(df)
+
+print("After Interpolation")
+
+df.interpolate(method='linear', inplace=True)
+
+print(df)
+
+
+"""
+use case 
+    1-timer series data 
+    2-numeric data with trends 
+    3-avoid dropping rows 
+    4- can't use in catagorical data 
+    5-sometimes the interpolation calculation doesn't work as expected
+
+"""
